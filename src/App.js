b3c8ee7b4c19646f484c96dca61fbeb23e42e712
Evyatar Haim <evyataros1@gmail.com>
2020-11-19 20:59:45 +0200

    building the header and fetching disease.sh-API to get a list of countries for a dropdown menu

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import { FormControl, MenuItem, Select } from '@material-ui/core';
+import styled from 'styled-components';
 
 function App() {
+ 
+  const [countries, setCountries] = useState(['USA', 'UK', 'INDIA']);
+
   return (
-    <div className="App">
-      
-      hfh
-    </div>
+    <Div className="App">
+      <div className="header">
+      <h1>COVID-19 TRACKER</h1>
+      <FormControl className="app_dropdown">
+        <Select
+           variant="outlined"
+           value="abc"
+           >
+             {countries.map(country => (
+            <MenuItem value={country}>{country}</MenuItem>
+             ))}
+             {/*
+             <MenuItem value="worldwide">WorldWide</MenuItem>
+             <MenuItem value="worldwide">WorldWide</MenuItem>
+             <MenuItem value="worldwide">WorldWide</MenuItem> */}
+           </Select>
+      </FormControl>
+      </div>
+    </Div>
   );
 }
 
 export default App;
+
+const Div = styled.div`
+.header{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+`
